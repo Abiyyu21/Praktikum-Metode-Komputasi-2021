a74247cbbfc73164e29b8cc925206c977aba9030
Biyyu Gf 20 <82901563+Abiyyu21@users.noreply.github.com>
2021-04-22 06:39:28 +0700

    Add files via upload

---
+++ Program Pendek Awal .py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+print("Hallo python!!!")
+
+
+# In[10]:
+
+
+#========================================#
+#                                        #
+#           Assalamualaikum              #
+#     Nama: Abiyyu Mahfuzh Raihan        #
+#      Program Studi: Geofisika          #
+#    by:abiyyu.mraihan@mail.ugm.ac.id    #
+#                                        #
+#========================================#
+
+
+# In[ ]:
+
+
+
+
